0fa788cd978ac7665824a9a94a8681f3fa94955a
ArllanSantana <adss5@aluno.ifal.edu.br>
2024-02-10 19:36:48 -0300

    Hello, World!

---
+++ src/main/java/org/spyg/spyguessingapi/SpyguessingApiApplication.java
@@ -0,0 +1,13 @@
+package org.spyg.spyguessingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpyguessingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpyguessingApiApplication.class, args);
+    }
+
+}
